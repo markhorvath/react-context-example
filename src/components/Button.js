5e08b9d6d72170d6ad6f8bea0c02a14e6ec01f25
markhorvath <markhorvath7@gmail.com>
2020-03-04 18:44:49 -0500

    context Provider, Consumer, notes

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,42 @@
 import React from 'react';
 import LanguageContext from '../context/LanguageContext';
+import ColorContext from '../context/ColorContext';
 
 class Button extends React.Component {
     //this is how we hook up a context object to a class component, contextType is a specific name that must b used
-    static contextType = LanguageContext;
+    // static contextType = LanguageContext;
     //static basically adds a property to the class itself, could also use Button.contextType = Language Context instead after creating the class (outside the class)
 
+
+    //WE USE CONSUMER ANY TIME WE WANT TO GET INFORMATION OUT OF MULTIPLE DIFFERENT CONTEXT OBJECTS
+    //INSIDE A SINGLE COMPONENT.
+
+    //USING THIS.CONTEXT IS USED WHEN WE ACCESS A SINGLE CONTEXT OBJECT IN A SINGLE COMPONENENT
+
+    //helper func for Consumer
+    renderSubmit(value) {
+        return value === 'english' ? 'Submit' : 'Voorleggen';
+    }
+
     render() {
-        console.log(this.context);
+        //THIS IS FOR USING THIS.CONTEXT
+        // const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
+        // return (
+        //     <button className="ui button primary">{text}</button>
+        // )
+
+        //providing a function as a child to a react component
+        //This could be turned into a JSX helper function
         return (
-            <button className="ui button primary">Submit</button>
+            <ColorContext.Consumer>
+            {(color) =>
+                <button className={`ui button ${color}`}>
+                    <LanguageContext.Consumer>
+                        {(value) => this.renderSubmit(value)}
+                    </LanguageContext.Consumer>
+                </button>
+            }
+            </ColorContext.Consumer>
         )
     }
 }
--- src/context/LanguageContext.js
+++ src/context/LanguageContext.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+// console.log(React.createContext('english'));
 
 //passing a paramater here is setting up a default for the context object
 export default React.createContext('english');
